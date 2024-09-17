bbf7f3a5fc40da4cbf36853d5ad793dc999350ea
Dewith <dewithmiramon@gmail.com>
2024-09-17 03:06:50 -0500

    🛠️ Create data preprocessing notebook

--- src/utils/styling.py
+++ src/utils/styling.py
@@ -1,43 +1,11 @@
 """Script that creates the styling utils."""
 
-import pandas as pd
 import seaborn as sns
 
 
-def get_palette():
+def make_palette() -> dict:
     """
     Get the color palette used for styling.
-
-    Returns
-    -------
-    tuple
-        A tuple containing the color palette and other styling colors.
-
-        The color palette is a list of color codes used for different elements.
-        The other styling colors include:
-        - lines: The color code for lines.
-        - text: The color code for regular text.
-        - text_lighter: The color code for lighter text.
-        - text_darker: The color code for darker text.
-        - background: The color code for the background.
-    """
-    green = '#66b392'
-    orange = '#f4a08f'
-    purple = '#c696de'
-    blue = '#739fd8'
-    palette = [green, orange, purple, blue]
-
-    text_darker = '#282828'
-    text = '#918c88'
-    text_lighter = '#b2afac'
-    lines = '#dad9d7'
-    background = '#faf7f4'
-    return palette, lines, text, text_lighter, text_darker, background
-
-
-def apply_styling():
-    """
-    Apply styling to the pandas and seaborn libraries.
 
     Returns
     -------
@@ -50,24 +18,58 @@ def apply_styling():
         - 'text_lighter': A lighter shade of the text color.
         - 'background': The background color of the plots.
     """
-    pd.set_option('display.max_columns', 100)
-    pd.set_option('display.max_rows', 100)
-    pd.set_option('display.float_format', lambda x: f'{x:.2f}')
-    pd.set_option('display.max_colwidth', 50)
+    green = '#66b392'
+    orange = '#f4a08f'
+    purple = '#c696de'
+    blue = '#739fd8'
+    palette = [green, orange, purple, blue]
 
-    palette, lines, text, text_lighter, text_darker, background = get_palette()
+    text_darker = '#282828'
+    text = '#918c88'
+    text_lighter = '#b2afac'
+    lines = '#dad9d7'
+    background = '#faf7f4'
+    return {
+        'palette': palette,
+        'lines': lines,
+        'text': text,
+        'text_darker': text_darker,
+        'text_lighter': text_lighter,
+        'background': background,
+    }
+
+
+def apply_styling(colors: dict = None):
+    """
+    Apply styling to the pandas and seaborn libraries.
+
+    Parameters
+    ----------
+    colors : dict, optional
+        A dictionary containing the colors to use for styling. If not provided,
+        the default color palette will be used. The dictionary should contain
+        the following keys:
+        - 'palette': The color palette used for plotting.
+        - 'lines': The color of the lines in the plots.
+        - 'text': The color of the text in the plots.
+        - 'text_darker': A darker shade of the text color.
+        - 'text_lighter': A lighter shade of the text color.
+        - 'background': The background color of the plots.
+    """
+    colors = colors or make_palette()
+
     params = {
         'figure.figsize': (6, 4),
         'figure.dpi': 200,
         'savefig.pad_inches': 0.3,
-        'figure.facecolor': background,
-        'axes.facecolor': background,
-        'axes.edgecolor': lines,
-        'axes.labelcolor': text,
-        'axes.titlecolor': text_darker,
-        'text.color': text,
-        'xtick.color': text_lighter,
-        'ytick.color': text_lighter,
+        'figure.facecolor': colors.get('background'),
+        'axes.facecolor': colors.get('background'),
+        'axes.edgecolor': colors.get('lines'),
+        'axes.labelcolor': colors.get('text'),
+        'axes.titlecolor': colors.get('text_darker'),
+        'text.color': colors.get('text'),
+        'xtick.color': colors.get('text_lighter'),
+        'ytick.color': colors.get('text_lighter'),
         'font.size': 9,
         'axes.titlesize': 12,
         'axes.labelsize': 10,
@@ -79,12 +81,6 @@ def apply_styling():
         'axes.linewidth': 0.6,
         'axes.grid': False,
     }
-    sns.set_theme(context='notebook', style='white', palette=palette, rc=params)
-    return {
-        'palette': palette,
-        'lines': lines,
-        'text': text,
-        'text_darker': text_darker,
-        'text_lighter': text_lighter,
-        'background': background,
-    }
+    sns.set_theme(
+        context='notebook', style='white', palette=colors.get('palette'), rc=params
+    )
